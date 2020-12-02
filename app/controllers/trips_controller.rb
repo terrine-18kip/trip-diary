cd3f9cfb34d97a976e4417c35fb290b8b700a229
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-12-02 11:43:23 +0900

    ルーティング、アクションの定義

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,11 @@
 class TripsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'trips#index'
   resources :users, only: [:edit, :update]
+  resources :trips, only: [:index, :new, :create]
 end
