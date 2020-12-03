b70f1f427e458e525cc750fc91fc9192875a190d
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-12-03 11:21:16 +0900

    旅程編集、削除機能の実装

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,9 @@
 class TripsController < ApplicationController
+  before_action :set_trip, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
+
+  helper_method :editor_user?
+
   def index
     @trips = Trip.all
   end
@@ -17,12 +22,43 @@ class TripsController < ApplicationController
   end
 
   def show
-    @trip = Trip.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @trip.update(trip_params)
+      redirect_to trip_path(@trip.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
   end
 
   private
 
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
   def trip_params
     params.require(:trip).permit(:title, :start_date, :end_date, :memo, user_ids: [])
+  end
+
+  def editor_user?
+    editor_ids = []
+    @trip.users.each do |user|
+      editor_ids << user.id
+    end
+    return editor_ids.include?(current_user.id)
+  end
+
+  def move_to_index
+    unless editor_user?
+      redirect_to action: :index
+    end
   end
 end
