a01048a056397d63a6fa843c3400710dc5a3489c
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-12-04 20:41:05 +0900

    plan#newアクションの定義

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,4 +1,10 @@
 class SpotsController < ApplicationController
+  def new
+    @spot = Spot.new
+    @plan = Plan.find(params[:plan_id])
+    @trip = Trip.find(params[:trip_id])
+  end
+
   def create
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :trips do
     resources :plans, only: [:create, :destroy] do
-      resources :spots, only: [:create, :destroy]
+      resources :spots, only: [:new, :create, :destroy]
     end
   end
 end
