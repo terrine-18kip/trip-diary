fe076e78bd8e257f96bbe57475bb9e23a97998ce
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-12-07 18:37:33 +0900

    ユーザー表示機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
+  def show
+    @trips = @user.trips
+  end
+
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to root_path
       bypass_sign_in(@user)
@@ -14,6 +18,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:image, :name, :email, :password)
