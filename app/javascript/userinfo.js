08e931813f0894d4b0443b9aedfe8687c64b6bf3
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-12-08 14:40:38 +0900

    プルダウンメニューの実装

---
+++ app/javascript/userinfo.js
@@ -0,0 +1,23 @@
+function pullDown() {
+
+  const pullDownbutton = document.getElementById("user-info")
+  const pullDownParents = document.getElementById("pull-down")
+
+  pullDownbutton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color:none;")
+  })
+
+  pullDownbutton.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color:none;")
+  })
+
+  pullDownbutton.addEventListener('click', function(){
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+    }
+  })
+}
+
+window.addEventListener('load', pullDown)
