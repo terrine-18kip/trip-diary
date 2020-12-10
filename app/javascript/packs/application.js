7bee0aa0c8ac9923a192511ef7099b1ca1354e1c
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-12-10 17:00:49 +0900

    jQueryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("channels")
 require("./preview")
 require("userinfo")
 require("member")
+require('jquery')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
