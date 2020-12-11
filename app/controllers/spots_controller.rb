87cf2ee36b268ac3ead53742b3842e494aeb8533
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-12-11 19:03:02 +0900

    モーダルフォームの実装

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,4 +1,6 @@
 class SpotsController < ApplicationController
+  before_action :set_spot, only: [:edit, :update, :destroy]
+
   def new
     @spot = Spot.new
     @plan = Plan.find(params[:plan_id])
@@ -9,37 +11,28 @@ class SpotsController < ApplicationController
   def create
     @spot = Spot.new(spot_params)
     if @spot.save
-      redirect_to trip_path(@spot.plan.trip.id)
+      redirect_to edit_trip_path(@spot.plan.trip.id)
     else
       render :new
     end
   end
 
   def edit
-    @spot = Spot.find(params[:id])
-    @plan = Plan.find(params[:plan_id])
-    @trip = Trip.find(params[:trip_id])
     redirect_to root_path unless editor_user?
   end
 
   def update
-    @spot = Spot.find(params[:id])
-    @plan = Plan.find(params[:plan_id])
-    @trip = Trip.find(params[:trip_id])
     if @spot.update(spot_params)
-      redirect_to trip_path(@spot.plan.trip.id)
+      redirect_to edit_trip_path(@trip.id)
     else
       render :edit
     end
   end
 
   def destroy
-    @spot = Spot.find(params[:id])
-    @plan = Plan.find(params[:plan_id])
-    @trip = Trip.find(params[:trip_id])
     if editor_user?
       @spot.destroy
-      redirect_to trip_path(@spot.plan.trip.id)
+      redirect_to edit_trip_path(@trip.id)
     else
       redirect_to root_path
     end
@@ -52,6 +45,12 @@ class SpotsController < ApplicationController
   end
 
   private
+
+  def set_spot
+    @spot = Spot.find(params[:id])
+    @plan = Plan.find(params[:plan_id])
+    @trip = Trip.find(params[:trip_id])
+  end
 
   def spot_params
     params.require(:spot).permit(:name, :category_id, :start_time, :end_time, :fee, :link, :memo, :row_order_position).merge(plan_id: params[:plan_id], trip_id: params[:trip_id])
