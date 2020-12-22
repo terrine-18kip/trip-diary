4afbb4a5f9cb3b42462b95335629e88e3c3d9df6
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-12-22 14:16:18 +0900

    bootstrapの設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("channels")
 require("./preview")
 require("userinfo")
 require('jquery')
+require('popper.js')
+require('bootstrap')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,7 +20,3 @@ require('jquery')
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-import 'popper.js/dist/popper.js'
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
